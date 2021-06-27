6c19ac63a3ca95737a69d173fe6cd3d3baea8f38
titipata <my.titipat@gmail.com>
2021-06-27 13:09:25 +0700

    Update the contact page again

--- website/src/pages/contact.js
+++ website/src/pages/contact.js
@@ -24,20 +24,22 @@ const ContactPage = ({
           <div className="primary-content">
             <b>Undergraduate students: </b>
             Please contact Titipat to arrange a short meeting to discuss
-            about research or potential projects
+            about research and/or potential projects.
           </div>
           <div className="primary-content">
             <b>Graduate students: </b>
             Due to limitation of the Mahidol university's rule, I cannot hire graduate
             students from the department yet. If you are interested in doing research with Titipat,
-            please email and arrange a meeting. You can potentially apply to Vidyasirimedhi Institute
-            of Science and Technology (VISTEC), Thailand, where Titpat can co-advise.
+            please email and arrange a discussion before applying. You can potentially apply to 
+            Department of Biomedical engineering at Mahidol University or
+            Vidyasirimedhi Institute of Science and Technology (VISTEC),
+            Thailand, where Titpat can co-advise.
           </div>
           <div className="primary-content">
             <b>Other positions: </b>
             I am always actively looking for interns and developers. If you are interested
-            in doing internship or other potions such as developers, please also do not hesitate
-            to email Titipat.
+            in doing internship or other positions such as developers, please also do not hesitate
+            to email Titipat to discuss.
           </div>
           <div className="primary-content">
             <b>Office: </b>
