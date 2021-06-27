e1251b4e5ea624cfcd55b5931b2c5f58824e67c1
tulakann <tulakan.r@gmail.com>
2021-06-27 23:19:59 +0700

    feat(resources): done /resources

---
+++ website/src/components/resource-link.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const ResourceLink = ({ resource }) => (
+  <article className="card ">
+    {/* <Link to={post.frontmatter.path}>
+      {!!post.frontmatter.thumbnail && (
+        <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
+      )}
+    </Link> */}
+    <header>
+      <h2 className="post-title">
+        <a href={resource.url} target="_blank" className="post-link">
+          {resource.name}
+        </a>
+      </h2>
+      <div className="post-meta">{resource.description}</div>
+    </header>
+  </article>
+)
+export default ResourceLink
