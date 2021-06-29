3ce47de55fb9aa7896a832213572cfc2b0d6cdb2
tulakann <tulakan.r@gmail.com>
2021-06-29 21:24:23 +0700

    fix: correct typos

--- website/src/components/navigation.js
+++ website/src/components/navigation.js
@@ -3,7 +3,7 @@ import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
-  <nav className="navigation"> 
+  <nav className="navigation">
     <Link to="/people">People</Link>
     <Link to="/publications">Publications</Link>
     <Link to="/blog">Blogs</Link>
