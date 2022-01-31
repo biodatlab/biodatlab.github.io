0bbe272f843236e81abd6e4eb59b7be6f38993ad
tulakann <tulakan.r@gmail.com>
2022-01-31 22:39:29 +0700

    feat: implement sending email using emailjs

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,16 @@
-import React from "react"
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
-import HelmetWrapper from "../components/helmetWrapper"
+import emailjs, { init } from "@emailjs/browser";
+import { graphql } from "gatsby";
+import React, { useEffect, useState } from "react";
+import HelmetWrapper from "../components/helmetWrapper";
+import Layout from "../components/layout";
 
-const ContactPage = ({
-  data: {
-    site
-  },
-}) => {
+const ContactPage = ({ data: { site } }) => {
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    init("user_LLZGUBFaiQGwK3qDYrTrV");
+  }, []);
+
   return (
     <Layout>
       <HelmetWrapper
@@ -18,68 +21,110 @@ const ContactPage = ({
         <div>
           <h1 className="post-title">Contact us</h1>
           <div className="primary-content">
-            If you are interested to join the lab or collaborate, please do not hesitate to send
-            Titipat an email (my.titipat [AT] gmail.com) or using the form in this page &rarr;
+            If you are interested to join the lab or collaborate, please do not
+            hesitate to send Titipat an email (my.titipat [AT] gmail.com) or
+            using the form in this page &rarr;
           </div>
           <div className="primary-content">
             <b>Undergraduate students: </b>
-            Please contact Titipat to arrange a short meeting to discuss
-            about research and/or potential projects.
+            Please contact Titipat to arrange a short meeting to discuss about
+            research and/or potential projects.
           </div>
           <div className="primary-content">
             <b>Graduate students: </b>
-            Due to limitation of the Mahidol university's rule, I cannot hire graduate
-            students yet. If you are interested in doing research,
-            please send a CV with a brief research interest. Then you can arrange a discussion before applying.
-            You can potentially apply to the Department of Biomedical engineering at Mahidol University,
-            Vidyasirimedhi Institute of Science and Technology (VISTEC),
-            or Chulalongkorn University, Thailand, where I can co-advise.
-            There are multiple options if you are interested.
+            Due to limitation of the Mahidol university's rule, I cannot hire
+            graduate students yet. If you are interested in doing research,
+            please send a CV with a brief research interest. Then you can
+            arrange a discussion before applying. You can potentially apply to
+            the Department of Biomedical engineering at Mahidol University,
+            Vidyasirimedhi Institute of Science and Technology (VISTEC), or
+            Chulalongkorn University, Thailand, where I can co-advise. There are
+            multiple options if you are interested.
           </div>
           <div className="primary-content">
-            <b>Other positions: </b>
-            I am always actively looking for interns and developers. If you are interested
-            in doing internship or other positions such as developers, please also do not hesitate
-            to email Titipat to discuss.
+            <b>Other positions: </b>I am always actively looking for interns and
+            developers. If you are interested in doing internship or other
+            positions such as developers, please also do not hesitate to email
+            Titipat to discuss.
           </div>
           <div className="primary-content">
             <b>Office: </b>
-            Mahidol University,
-            Engineering Building 3, 3rd Floor
-            999 Phutthamonthon 4 Road,
-            Salaya, Nakhon Pathom, Thailand 73170
+            Mahidol University, Engineering Building 3, 3rd Floor 999
+            Phutthamonthon 4 Road, Salaya, Nakhon Pathom, Thailand 73170
           </div>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
-            <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
-            </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
-            </div>
+          <form
+            className="form-container"
+            onSubmit={e => {
+              e.preventDefault();
+
+              const formdata = new FormData(e.target);
+
+              const name = formdata.get("w3lName");
+              const replyTo = formdata.get("w3lSender");
+              const subject = formdata.get("w3lSubject");
+              const message = formdata.get("w3lMessage");
+
+              emailjs
+                .send("service_31gu986", "template_ek909s9", {
+                  name,
+                  replyTo,
+                  subject,
+                  message
+                })
+                .then(
+                  () => {
+                    setSubmitted(true);
+                  },
+                  rej => {
+                    console.log("failed with result:", rej);
+                  }
+                );
+            }}
+          >
+            {submitted ? (
+              <p style={{ margin: 0, lineHeight: "1.5em" }}>
+                Your message has successfully been sent to Titipat! It generally
+                takes one to two days for him to reply.
+              </p>
+            ) : (
+              <>
+                <div>
+                  <label htmlFor="w3lName">Name</label>
+                  <input type="text" name="w3lName" id="w3lName" />
+                </div>
+                <div>
+                  <label htmlFor="w3lSender">Email</label>
+                  <input type="email" name="w3lSender" id="w3lSender" />
+                </div>
+                <div>
+                  <label htmlFor="w3lSubject">Subject</label>
+                  <input type="text" name="w3lSubject" id="w3lSubject" />
+                </div>
+                <div>
+                  <label htmlFor="w3lMessage">Message</label>
+                  <textarea name="w3lMessage" id="w3lMessage"></textarea>
+                </div>
+                <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                  <input
+                    type="submit"
+                    className="button -primary"
+                    style={{ marginRight: 0 }}
+                  />
+                </div>
+              </>
+            )}
           </form>
         </div>
       </div>
     </Layout>
-  )
-}
-export default ContactPage
+  );
+};
+
+export default ContactPage;
 export const pageQuery = graphql`
-  query ContactPageQuery{
+  query ContactPageQuery {
     site {
       siteMetadata {
         title
@@ -87,4 +132,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
