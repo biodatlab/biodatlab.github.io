5d236883552ca6aedb2665e9297ef626bdfa4410
titipata <my.titipat@gmail.com>
2023-03-05 16:16:20 +0000

    Deploying to gh-pages from @ biodatlab/biodatlab.github.io@f217bc6f4245a9e861310a8910a68bef91f61adf 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9e6c97f33d004ae5b5bb.js"
+    "url": "webpack-runtime-653f3e91b8943155f5c3.js"
   },
   {
     "url": "framework-31b40719cb76dbe7926d.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "31f8762b587bde1eb35990973e8c1c45"
+    "revision": "e73c820b10c65ffd1bc3d55aef67b21f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
