f06ecb3ad6db8abbfdfd4a1a7c4205c9d695d6b8
Titipat Achakulvisut <my.titipat@gmail.com>
2024-08-20 20:08:59 +0700

    Adding copyright notice on publication page

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -28,6 +28,17 @@ const PublicationPage = ({
         not have access to any publication, please email.
       </div>
       <div className="primary-content">{Publication}</div>
+      <hr />
+      <div className="primary-content">
+        Copyright notice. The documents listed here are available for downloading and have been
+        provided as a means to ensure timely dissemination of scholarly and
+        technical work on a noncommercial basis. Copyright and all rights therein
+        are maintained by the authors or by other copyright holders, notwithstanding
+        that they have offered their works here electronically. It is understood that
+        all persons copying this information will adhere to the terms and constraints
+        invoked by each author’s copyright. These works may not be re-posted without
+        the explicit permission of the copyright holder.
+      </div>
     </Layout>
   );
 };
